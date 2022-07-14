a06943dfc63d8638c616367c588a6c21a5ea8f01
Arthur Dehgan <arthurdehgan@gmail.com>
2022-07-14 18:37:13 -0400

    updated the code to compute all the metrics in one classification

--- setup.py
+++ setup.py
@@ -11,18 +11,19 @@ except:
     print("Failed to retrieve the current version, defaulting to 0")
     version = "0"
 
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
-
 setuptools.setup(
     name="imbalance",
     version=version,
     author="The CoCo Lab",
     description="An exploration of data imbalance",
     packages=setuptools.find_packages(),
-    install_requires = REQUIREMENTS,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+    dependency_links=[
+        'git+ssh://git@github.com/arthurdehgan/NeuroPy-MLToolbox.git#egg=mlneurotools',
     ],
     python_requires='==3.8.*',
 )
