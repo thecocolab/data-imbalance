a965a9e0ea3016dd957fe8f6531700c4ef5aca51
Yorguin José Mantilla Ramos <yjmantilla@gmail.com>
2022-07-15 16:24:38 -0400

    minor changes topomap

--- scripts/eeg_imbalance_topomaps.py
+++ scripts/eeg_imbalance_topomaps.py
@@ -5,10 +5,11 @@ from imbalance import viz
 import pickle
 from sklearn.linear_model import LogisticRegression
 import os
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 
 pipeline_path='data/eeg_%%.pickle'
-features_path ="data/eeg_features.npy"
+features_path ="data/eeg_features_unscaled.npy"
 chans = get_info().ch_names
 
 rois = [[x] for x in chans]
@@ -16,7 +17,7 @@ rois_names = chans
 
 if not os.path.isfile(features_path):
     # load or generate dataset
-    x, y, groups = eegbci('data',roi=None)
+    x, y, groups = eegbci('data',roi=None,scale=False)
     np.save(features_path,dict(x=x, y=y, groups=groups))
 else:
     features = np.load(features_path,allow_pickle=True).item()
@@ -26,14 +27,18 @@ else:
 
 for roi,roi_name in zip(rois,rois_names):
     this_pipe = pipeline_path.replace('%%',roi_name)
+    idxs = [chans.index(c) for c in roi]
+    curr_x = x
     if not os.path.isfile(this_pipe):
         pl = Pipeline(
             x,
             y,
             groups,
-            dataset_balance = np.linspace(0.1, 0.9, 25),
+            dataset_balance = [0.1,0.5,0.9],
             classifiers = ['rf',"lda","svm",LogisticRegression(max_iter=1000)],
             metrics = ["roc_auc","accuracy", "f1", "balanced_accuracy"],
+            n_permutations = 0,
+            n_init = 10,
         )
         # fit and evaluate classifiers on dataset configurations
         pl.evaluate()
