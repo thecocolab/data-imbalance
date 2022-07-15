1de9f6ffefcaf52c6006586476b0dcd3cf4f5e95
Yorguin José Mantilla Ramos <yjmantilla@gmail.com>
2022-07-15 12:06:15 -0400

    eeg compute canada scripts

--- scripts/eeg_imbalance_analysis.py
+++ scripts/eeg_imbalance_analysis.py
@@ -20,50 +20,53 @@ cvs = [KFold(n_splits=5), StratifiedKFold(n_splits=5), StratifiedGroupKFold(n_sp
 if len(sys.argv) > 1:
     print('cmd entry:', sys.argv)
     cvs_idx = int(sys.argv[-1])
+    cvs_idx = [cvs_idx]
 else:
-    cvs_idx = 2 # because we have groups by default
+    cvs_idx = [1,0,2] # because we have groups by default
 
-cv = cvs[cvs_idx]
+for ccc in cvs_idx:
+    cv = cvs[ccc]
 
-for n_features in ['single','multi']:
-    chans = get_info().ch_names
+    for n_features in ['single','multi']:
+        chans = get_info().ch_names
 
-    pipeline_path=f'data/eeg_roi_{n_features}_{cvsnames[cvs_idx]}.pickle'
-    features_path =f"data/eeg_features_{n_features}_{cvsnames[cvs_idx]}.npy"
+        pipeline_path=f'data/eeg_roi_{n_features}_{cvsnames[ccc]}.pickle'
+        features_path =f"data/eeg_features_{n_features}.npy"
 
-    if not os.path.isfile(features_path):
-        x, y, groups = eegbci('data',roi=lambda x: x[0] in ['P','O'],n_features=n_features)
-        np.save(features_path,dict(x=x, y=y, groups=groups))
-    else:
-        print("loading features from a previous run")
-        features = np.load(features_path,allow_pickle=True).item()
-        x, y, groups = features["x"] , features["y"] , features["groups"]
-
-
-    if not os.path.isfile(pipeline_path):
-        pl = Pipeline(
-            x,
-            y,
-            groups,
-            dataset_balance = np.linspace(0.1, 0.9, 25),
-            classifiers = ["lda","svm",LogisticRegression(max_iter=1000),RandomForestClassifier(n_estimators=25)],
-            n_permutations = 100,
-            n_init = 10,
-        )
-        # fit and evaluate classifiers on dataset configurations
-        pl.evaluate()
-
-        # Store data (serialize)
-        with open(pipeline_path, 'wb') as handle:
-            pickle.dump(pl, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    else:
-        print("loading pipeline from a previous run")
-
-        pl = pickle.load(open(pipeline_path,"rb"))
+        if not os.path.isfile(features_path):
+            x, y, groups = eegbci('data',roi=lambda x: x[0] in ['P','O'],n_features=n_features)
+            np.save(features_path,dict(x=x, y=y, groups=groups))
+        else:
+            print("loading features from a previous run")
+            features = np.load(features_path,allow_pickle=True).item()
+            x, y, groups = features["x"] , features["y"] , features["groups"]
 
 
-# clfs = [key for key,val in viz.CLASSIFIERS.items()]
-# for clf in clfs:
-#     viz.metric_balance(pl,clf)
+        if not os.path.isfile(pipeline_path):
+            pl = Pipeline(
+                x,
+                y,
+                groups,
+                dataset_balance = np.linspace(0.1, 0.9, 25),
+                classifiers = ["lda","svm",LogisticRegression(max_iter=1000),RandomForestClassifier(n_estimators=25)],
+                n_permutations = 100,
+                n_init = 10,
+                cross_validation=cvs[ccc]
+            )
+            # fit and evaluate classifiers on dataset configurations
+            pl.evaluate()
 
-# viz.data_distribution(pl)
+            # Store data (serialize)
+            with open(pipeline_path, 'wb') as handle:
+                pickle.dump(pl, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        else:
+            print("loading pipeline from a previous run")
+
+            pl = pickle.load(open(pipeline_path,"rb"))
+
+
+    # clfs = [key for key,val in viz.CLASSIFIERS.items()]
+    # for clf in clfs:
+    #     viz.metric_balance(pl,clf)
+
+    # viz.data_distribution(pl)
