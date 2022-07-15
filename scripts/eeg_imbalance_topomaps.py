ddff8befff9cb5a48146d68c2f6806e106a69d6d
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2022-07-15 16:42:42 -0300

    computations for topomap

--- scripts/eeg_imbalance_topomaps.py
+++ scripts/eeg_imbalance_topomaps.py
@@ -17,26 +17,31 @@ rois_names = chans
 
 if not os.path.isfile(features_path):
     # load or generate dataset
-    x, y, groups = eegbci('data',roi=None,scale=False)
+    x, y, groups = eegbci('data',roi=None,scale=False,n_features='multi')
     np.save(features_path,dict(x=x, y=y, groups=groups))
 else:
     features = np.load(features_path,allow_pickle=True).item()
     x, y, groups = features["x"] , features["y"] , features["groups"]
 
+
 # Tests : all chans, roi, chan by chan
 
 for roi,roi_name in zip(rois,rois_names):
+
     this_pipe = pipeline_path.replace('%%',roi_name)
     idxs = [chans.index(c) for c in roi]
-    curr_x = x
+    curr_x =  x.copy()[:,idxs]
+    scaler = StandardScaler()
+    curr_x = scaler.fit_transform(curr_x)
+    print(roi_name,idxs,curr_x.shape)
+
     if not os.path.isfile(this_pipe):
         pl = Pipeline(
-            x,
+            curr_x,
             y,
             groups,
             dataset_balance = [0.1,0.5,0.9],
             classifiers = ['rf',"lda","svm",LogisticRegression(max_iter=1000)],
-            metrics = ["roc_auc","accuracy", "f1", "balanced_accuracy"],
             n_permutations = 0,
             n_init = 10,
         )
