6f2296a69e2e2465075654a9129da8c1a95d366c
Arthur Dehgan <arthurdehgan@gmail.com>
2022-07-15 13:32:21 -0400

    few updates

--- scripts/meg_analysis.py
+++ scripts/meg_analysis.py
@@ -1,37 +1,24 @@
-import warnings
 import pickle
 import copy
 import os
 from itertools import product
-from typing import Sequence, List, Tuple, Union, Optional, Dict, Any
 import numpy as np
-from sklearn.base import clone, BaseEstimator
-from sklearn.model_selection import (
-    BaseCrossValidator,
-    KFold,
-    GroupKFold,
-    permutation_test_score,
-)
-from sklearn.metrics import SCORERS
-from sklearn.linear_model import LogisticRegression
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
 from camcan.dataloaders import create_datasets
 from torch.utils.data import ConcatDataset, DataLoader
-from scipy.io import savemat
 from joblib import Parallel, delayed
+from sklearn.preprocessing import scale
 from imbalance.pipeline import Pipeline
 
 
-def compute_and_save_results(x, y, groups, clf, data_path, elec):
-    pl = Pipeline(x, y, groups, classifiers=[clf])
+def compute_and_save_results(x, y, groups, data_path, elec, band_name):
+    classifiers = ['lr', 'svm', 'rf', 'lda']
+    pl = Pipeline(x, y, groups, classifiers=classifiers, n_permutations=0, n_init=1)
     pl.evaluate()
     save_path = os.path.join(data_path, "results")
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     with open(
-        os.path.join(save_path, f"MEG_PSD_imbalance_{clf}_{elec}.pckl"),
+        os.path.join(save_path, f"MEG_{band_name}_imbalance_{elec}.pckl"),
         "wb",
     ) as fp:
         pickle.dump(copy.deepcopy(pl), fp, protocol=pickle.HIGHEST_PROTOCOL)
@@ -40,27 +27,33 @@ def compute_and_save_results(x, y, groups, clf, data_path, elec):
 if __name__ == "__main__":
     import argparse
 
+    BANDS = ["delta", "theta", "alpha", "beta", "gamma1", "gamma2", "gamma3"]
+
     parser = argparse.ArgumentParser(
         "Compute the scores for imbalanced datasets and saves them."
     )
     parser.add_argument(
-        "--elec-index", type=int, default=1, help="The elec index, between 0 and 102."
+        "--data-path", type=str, default="/home/arthur/data/camcan", help="The path where the data can be found"
+    )
+    parser.add_argument(
+        "--band", type=str, default="alpha", choices=BANDS, help="The frequency band to use for single feature classification"
     )
     args = parser.parse_args()
-    elec_index = args.elec_index
+    band_name = args.band
+    data_path = args.data_path
 
-    data_path = "/home/arthur/data/camcan"
     train_size = 0.8
     seed = 42
     dattype = "passive"
     eventclf = True
     epoched = True
     s_freq = 500
+    band = band_name.index(band_name)
 
     datasets = create_datasets(
         data_path,
         train_size,
-        max_subj=200,
+        max_subj=50,
         seed=seed,
         dattype=dattype,
         eventclf=eventclf,
@@ -71,13 +64,17 @@ if __name__ == "__main__":
     dataloader = DataLoader(dataset, batch_size=len(dataset))
     x, y, groups = next(iter(dataloader))
     x = x[:, [1, 2]].mean(axis=1)  # averages the psd values for both gradiometers
-    x = x[..., 1]  # Only using the alpha frequency band
+    x = x[..., band]  # Only using the alpha frequency band. comment this line for multifeature.
+    x = x.numpy()
 
-    classifiers = ["lr", "svm", "lda", "rf"]
+    for g in np.unique(groups):
+        group_idx = np.where(groups == g)[0]
+        x[group_idx] = scale(x[group_idx])
 
+    # running all classifiers and 102 sensor locations gives us
     Parallel(n_jobs=-1)(
         delayed(compute_and_save_results)(
-            x[:, elec_index], y, groups, clf, data_path, elec_index
+            x[:, elec_index], y, groups, data_path, elec_index, band_name
         )
-        for clf in classifiers
+        for elec_index in range(102)
     )
