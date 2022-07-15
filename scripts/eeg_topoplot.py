3ca3de6db1e28610cca16a14c8155c7fd74946d3
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2022-07-15 18:58:51 -0300

    final topoplots

--- scripts/eeg_topoplot.py
+++ scripts/eeg_topoplot.py
@@ -8,7 +8,7 @@ info = get_info()
 chans = get_info().ch_names
 
 
-clfs = [key for key,val in viz.CLASSIFIERS.items()]
+clfs = ["svm"]#[key for key,val in viz.CLASSIFIERS.items()]
 metrics = [key for key,val in viz.METRIC.items()]
 
 clf_ = "svm"
@@ -17,12 +17,13 @@ met = "accuracy"
 for clf_ in clfs:
     for met in metrics:
         clf = viz.CLASSIFIERS[clf_]
-        outpath = f"data/eeg_topoplot_met-{met}_clf-{clf}.png"
+        outpath = f"data/topos_3balances_allCLFS/eeg_topoplot_met-{met}_clf-{clf}.png"
+        met_ = viz.METRIC[met]
 
         topobalances = [] # channels x balances
         for chan in chans:
 
-            pipeline_path = f'data\eeg_{chan}.pickle'
+            pipeline_path = f'data/topos_3balances_allCLFS/eeg_{chan}.pickle'
             pl = pickle.load(open(pipeline_path,"rb"))
             scores = pl.get(dataset_size="max", result_type="score")
             balances = np.array(list(scores.keys()))
@@ -34,13 +35,16 @@ for clf_ in clfs:
         topobalances=np.array(topobalances)
         vmin=0#topobalances.min()
         vmax=1#topobalances.max()
-        fig,axes = plt.subplots(nrows=2,ncols=balances.shape[0],sharey=True)
+        fig,axes = plt.subplots(nrows=1,ncols=balances.shape[0],sharey=True)
         for i in range(topobalances.shape[-1]):
-            im,cn = mne.viz.plot_topomap(topobalances[:,i],info,axes=axes[0][i],cmap='seismic',vmin=vmin,vmax=vmax,show=False)
-
-        cbar=plt.colorbar(mappable=im,ax=axes[1][1],format='%.1f',location='top')
-
-        plt.tight_layout()
-        plt.suptitle(outpath)
+            axes[i].set_title(f"R = {balances[i]}")
+            im,cn = mne.viz.plot_topomap(topobalances[:,i],info,contours=0,extrapolate='head',axes=axes[i],cmap='jet',vmin=vmin,vmax=vmax,show=False)
+        cbar_ticks = np.linspace(vmin,vmax,5)
+        cbar=plt.colorbar(mappable=im,ax=axes,ticks=cbar_ticks,format='%.1f',location='bottom')
+        cbar.set_label("Score")
+        if clf == 'SVC':
+            plt.suptitle(f"{met_} for SVM")
+        else:
+            plt.suptitle(f"{met_} for {clf}")
 
         fig.savefig(outpath)
